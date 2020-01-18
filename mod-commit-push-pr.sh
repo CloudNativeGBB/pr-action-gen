f8b0c1a5e66f52f8d24b2e9ba7dbc0656c553c7d
Joey Schluchter <jschluchter@gmail.com>
2020-01-18 11:10:33 -0500

    updates to test workflows

--- mod-commit-push-pr.sh
+++ mod-commit-push-pr.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 echo "" # see https://github.com/actions/toolkit/issues/168
-GH_USER=${INPUT_GHUSER}
-GH_USER_EMAIL=${INPUT_GHUSEREMAIL}
-GH_USER_NAME=${INPUT_GHUSERNAME}
-GH_USER_TOKEN=${INPUT_GHUSERTOKEN}
-GH_PKG_NAME=${INPUT_PKGNAME}
-GH_PKG_TOKEN=${INPUT_GHPKGTOKEN}
+GH_USER="${INPUT_GHUSER}"
+GH_USER_EMAIL="${INPUT_GHUSEREMAIL}"
+GH_USER_NAME="${INPUT_GHUSERNAME}"
+GH_USER_TOKEN="${INPUT_GHUSERTOKEN}"
+GH_PKG_NAME="${INPUT_PKGNAME}"
+GH_PKG_TOKEN="${INPUT_GHPKGTOKEN}"
 
 
 #CLONE REPO DOWN
 git clone https://$GH_USER:$GH_USER_TOKEN@github.com/CloudNativeGBB/app-baseline.git
 
 #GET THE LATEST TAG
-TAG=$(curl -sSk -H "Authorization: Bearer $GH_PKG_TOKEN" https://docker.pkg.github.com/v2/cloudnativegbb/arc-k8s-demos/$GH_PKG_NAME/tags/list?n=1 | jq '.tags[0]' | tr -d  '"')
+TAG=$(curl -sSk -H "Authorization: Bearer $GH_PKG_TOKEN" "https://docker.pkg.github.com/v2/cloudnativegbb/arc-k8s-demos/$GH_PKG_NAME/tags/list?n=1" | jq '.tags[0]' | tr -d  '"')
 
 #GIT MODS
 cd app-baseline
 git checkout -b $TAG
-sed -i -e "s|        - image: msftgbb/$GH_PKG_NAME.*|        - image: msftgbb/$GH_PKG_NAME:$TAG|g" k8s/${GH_PKG_NAME}.yaml
+sed -i -e "s|        - image: msftgbb/$GH_PKG_NAME.*|        - image: msftgbb/$GH_PKG_NAME:$TAG|g" "k8s/${GH_PKG_NAME}.yaml"
 git config --global user.email ${GH_USER_EMAIL}
 git config --global user.name ${GH_USER_NAME}
 git add .
